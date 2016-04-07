57744138ad1c95bc0466d252b2ff2df8ba580db6
Wynn Drahorad <wdrahora@bertvm.cs.uic.edu>
2016-04-07 10:05:07 -0500

    add project 6

---
+++ Project/6/Airport.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+/*
+*	Airport.java: Contains Airport class containing methods:
+*		-
+*
+*/
+
+
+class Airport {
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ Project/6/FileChain.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+/**
+*	FileChain.java: Contains FileChain class used to hold linked list of files currently in use.
+*
+*	FileChain.java contains methods:
+*		
+*
+*/
+
+
+class FileChain {
+	public static void main(String[] args) {
+
+	}
+}
